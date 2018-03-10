bf9d0cb62170738d2ff33d56ac0d81c7749e6520
Daniel Foster <danieljfoster87@gmail.com>
2018-03-10 13:03:39 -0500

    hopefully this works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -209,12 +209,14 @@ function myFunction(x) {
     //console.log("hit");
     for (var i = 0; i < letterButtons.length; i++) {
       letterButtons[i].classList.remove("btn-lg")};
+      letterButtons[i].classList.add("btn-sm")};
       
   } else {
     var letterButtons = document.getElementsByClassName("button-class");
     //console.log("hit");
     for (var i = 0; i < letterButtons.length; i++) {
       letterButtons[i].classList.add("btn-lg")};
+      letterButtons[i].classList.remove("btn-sm")};
   }
 }
 
